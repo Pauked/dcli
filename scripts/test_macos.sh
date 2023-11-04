cab5fd49cccc9f4196eb89f54984eb7e95a1a056
Paul Healey <code@healeys.dev>
2023-11-04 19:23:20 +0000

    Refactoring paths on macOS test script

--- scripts/test_macos.sh
+++ scripts/test_macos.sh
@@ -8,50 +8,52 @@
 # Written for macOS. Should run without errors.
 
 # Set top level variables
+base_path="$DROPBOX_PATH/Games/Doom"
 dcli_path="./target/debug/dcli"
 
 # Engine paths
-gzdoom_path="~/Dropbox/Games/Doom/Apps/GzDoom4100Mac/gzdoom.app"
+gzdoom_path="$base_path/Apps/GzDoom4111Mac/gzdoom.app"
 
 # IWAD paths
-doom_wad="~/Dropbox/Games/Doom/Maps/Doom.wad"
-doom2_wad="~/Dropbox/Games/Doom/Maps/Doom2.wad"
-tnt_wad="~/Dropbox/Games/Doom/Maps/Final Doom/Tnt.wad"
-heretic_wad="~/Dropbox/Games/Doom/Maps/Heretic.wad"
-hexen_wad="~/Dropbox/Games/Doom/Maps/Hexen.wad"
-hexdd_wad="~/Dropbox/Games/Doom/Maps/HexDD.wad"
+maps_path="$base_path/Maps"
+doom_wad="$maps_path/Doom.wad"
+doom2_wad="$maps_path/Doom2.wad"
+tnt_wad="$maps_path/Final Doom/Tnt.wad"
+heretic_wad="$maps_path/Heretic.wad"
+hexen_wad="$maps_path/Hexen.wad"
+hexdd_wad="$maps_path/HexDD.wad"
 
 # Delete the db so we have a clean slate (force will skip any confirmation prompts)
 $dcli_path reset --force
 
 # Init the app
-$dcli_path init "~/Dropbox/Games/Doom/Apps/" "~/Dropbox/Games/Doom/Maps/" "~/Dropbox/Games/Doom/Maps/" --force
+$dcli_path init "$base_path/Apps/" "$maps_path/" "$maps_path/" --force
 
 # Change menu mode
 $dcli_path set-app-settings --menu-mode simple
 $dcli_path set-app-settings --use-doomworld-api true
 
 # Add individual profiles
-$dcli_path add-profile "Classic Doom" $gzdoom_path $doom_wad
-$dcli_path add-profile "Classic Doom 2" $gzdoom_path $doom2_wad
-$dcli_path add-profile "Ancient Aliens" $gzdoom_path $doom2_wad --maps "aaliens.wad"
-$dcli_path add-profile "Sigil" $gzdoom_path $doom_wad --maps SIGIL_v1_21.wad,SIGIL_SHREDS.wad
-$dcli_path add-profile "Phobos Mission Control" $gzdoom_path $doom_wad --maps e1m4b.wad --args " -warp 1 4"
-$dcli_path add-profile "TNT: Revilution" $gzdoom_path "$tnt_wad" --maps "tntr.wad" --args " -deh ~/Dropbox/Games/Doom/Maps/Tntr/tntr.deh"
-$dcli_path add-profile "RAMP 2021" $gzdoom_path $doom2_wad --maps ramp.pk3 --args " -loadgame save03.zds"
-$dcli_path add-profile "UAC Ultra" $gzdoom_path $doom2_wad --maps uacultra.wad
-$dcli_path add-profile "Heretic" $gzdoom_path $heretic_wad
-$dcli_path add-profile "Hexen" $gzdoom_path $hexen_wad
-$dcli_path add-profile "Hexen: Deathkings of the Dark Citadel" $gzdoom_path $hexdd_wad
+$dcli_path add-profile "Classic Doom" "$gzdoom_path" "$doom_wad"
+$dcli_path add-profile "Classic Doom 2" "$gzdoom_path" "$doom2_wad"
+$dcli_path add-profile "Ancient Aliens" "$gzdoom_path" "$doom2_wad" --maps "aaliens.wad"
+$dcli_path add-profile "Sigil" "$gzdoom_path" "$doom_wad" --maps SIGIL_v1_21.wad,SIGIL_SHREDS.wad
+$dcli_path add-profile "Phobos Mission Control" "$gzdoom_path" "$doom_wad" --maps e1m4b.wad --args " -warp 1 4"
+$dcli_path add-profile "TNT: Revilution" "$gzdoom_path" "$tnt_wad" --maps "tntr.wad" --args " -deh $maps_path/Tntr/tntr.deh"
+$dcli_path add-profile "RAMP 2021" "$gzdoom_path" "$doom2_wad" --maps ramp.pk3 --args " -loadgame save03.zds"
+$dcli_path add-profile "UAC Ultra" "$gzdoom_path" "$doom2_wad" --maps uacultra.wad
+$dcli_path add-profile "Heretic" "$gzdoom_path" "$heretic_wad"
+$dcli_path add-profile "Hexen" "$gzdoom_path" "$hexen_wad"
+$dcli_path add-profile "Hexen: Deathkings of the Dark Citadel" "$gzdoom_path" "$hexdd_wad"
 
 # Set defaults
-$dcli_path set-default --engine $gzdoom_path
-$dcli_path set-default --iwad $doom2_wad
+$dcli_path set-default --engine "$gzdoom_path"
+$dcli_path set-default --iwad "$doom2_wad"
 $dcli_path set-default --profile "Sigil"
 
 # Set play settings
 $dcli_path set-play-settings --comp-level pr-boom-plus
-$dcli_path set-play-settings --config-file "~/Dropbox/Games/Doom/Apps/config.cfg"
+$dcli_path set-play-settings --config-file "$base_path/Apps/config.cfg"
 $dcli_path set-play-settings --fast-monsters true
 $dcli_path set-play-settings --no-monsters true
 $dcli_path set-play-settings --respawn-monsters true
